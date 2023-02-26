5b95f83bc6ed8e1cc009c11bf8f9b5eeaa15e31b
MarcoVercosa <marco2011sky@gmail.com>
2023-02-26 17:11:23 -0300

    new changes

--- src/pages/leitura/[versaoid]/[testamentoid]/[livroid]/[capitulo]/index.tsx
+++ src/pages/leitura/[versaoid]/[testamentoid]/[livroid]/[capitulo]/index.tsx
@@ -2,11 +2,21 @@ import { useRouter } from "next/router"
 
 export default function LeituraBiblia() {
 
+    // const versaoAmount = 1 // somente a Almeida revisada e atualizada
+    // const testamentoAmount = 2 //os dois testamentos
+    // const livroAmount = 66  //todos os livros Antigo e novo testamentos
+    // const capituloAmount = "solicitar api quantidade de capitulos que tem o livro. utilizar a FetchAPICapitulos()"
+
+    const versaoAmountTest = 1 // somente a Almeida revisada e atualizada
+    const testamentoAmountTest = 2 //os dois testamentos
+    const livroAmountTest = 66  //todos os livros Antigo e novo testamentos
+    const capituloAmountTest = 5 //somente os 5 capitulos de cada livro
+
     const router = useRouter()
     console.log(router.query)
     return (
         <h1>
-            Leitura
+            LeituraAAAAA
         </h1>
     )
 }
