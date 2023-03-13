fc75f81b11a2080d898633fa11b2dbef75fb6f8e
MarcoVercosa <marco2011sky@gmail.com>
2023-03-13 20:33:09 -0300

    IA Layout

--- src/pages/leitura/[versaoid]/[testamentoid]/[livroid]/[capitulo]/index.tsx
+++ src/pages/leitura/[versaoid]/[testamentoid]/[livroid]/[capitulo]/index.tsx
@@ -7,8 +7,6 @@ import ReadingPanel from "@/components/bible/COMPreadingContentBiblePanel"
 import Footer from "../../../../../../components/footer/COMPfooter"
 import Loading from '@/components/loading/COMPloading';
 
-
-
 const versao = 1 // somente a Almeida revisada e atualizada
 const todostestamentos = 2 //os dois testamentos
 const antigoTestamento = 1
@@ -25,7 +23,6 @@ interface IGetStaticProps {
     capitulo: string
 }
 
-
 export default function LeituraBiblia({ data }: { data: IBuscaConteudoLeitura }): JSX.Element {
     if (!data) {
         return (
@@ -38,7 +35,7 @@ export default function LeituraBiblia({ data }: { data: IBuscaConteudoLeitura })
     }
     return (
         <>
-            {data?.conteudo[0]?.conteudo &&
+            {data?.conteudo?.length > 1 &&
                 <Head>
                     <title>{data?.nomeLivro[0]?.livro_nome} {data?.capituloAtual} - Almeida revisada e atualizada - Vida da fonte</title>
                     <meta name="description" content={`${data?.conteudo[0]?.conteudo.substring(1, 90)} ...`}></meta>
@@ -53,35 +50,48 @@ export default function LeituraBiblia({ data }: { data: IBuscaConteudoLeitura })
 }
 export const getStaticPaths: GetStaticPaths = async () => {
     let paths = []
-    for (let testamento = 1; testamento <= antigoTestamento; testamento++) {
-        for (var livros = 1; livros <= livrosAntigoTestamento; livros++) {
-            let fetchCapitulos = await FetchAPICapitulosServerSide(versao, livros)//obtem a quantida de exata de capitulos do livro da vez
-            for (var capitulos = 1; capitulos <= fetchCapitulos[0].capitulo; capitulos++) {
-                paths.push({
-                    params: {
-                        versaoid: `${versao}`,
-                        testamentoid: `${testamento}`,
-                        livroid: `${livros}`,
-                        capitulo: `${capitulos}`
-                    }
-                })
+
+    try {
+        for (let testamento = 1; testamento <= antigoTestamento; testamento++) {
+            for (var livros = 1; livros <= livrosAntigoTestamento; livros++) {
+                let fetchCapitulos = await FetchAPICapitulosServerSide(versao, livros)//obtem a quantida de exata de capitulos do livro da vez
+                for (var capitulos = 1; capitulos <= fetchCapitulos[0].capitulo; capitulos++) {
+                    paths.push({
+                        params: {
+                            versaoid: `${versao}`,
+                            testamentoid: `${testamento}`,
+                            livroid: `${livros}`,
+                            capitulo: `${capitulos}`
+                        }
+                    })
+                }
             }
         }
+        return { paths, fallback: true }
+    } catch (err) {
+        paths.push({
+            params: {
+                versaoid: `0`,
+                testamentoid: `0`,
+                livroid: `0`,
+                capitulo: `0`
+            }
+        })
+        return { paths, fallback: true }
     }
-    return { paths, fallback: true }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }: any) => {
     const { versaoid, testamentoid, livroid, capitulo } = params as IGetStaticProps
     try {
         let response = await FetchConteudoServerSide(versaoid, testamentoid, livroid, capitulo) as IBuscaConteudoLeitura //busca conteudo conforme url da página
+        console.log(response)
         return {
             props: {
                 data: response
             }
         }
     } catch (error) {
-        console.log(error)
         return {
             props: {
                 data: []
